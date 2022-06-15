3985492d1373c3cbef7a69f799b658a9c10f688b
Krishna <libraryismyparadise@gmail.com>
2022-06-15 22:46:26 +0545

    Add js for mobile menu and nav link

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navItem = document.querySelector(".nav-item");
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navItem.classList.toggle("active");
+  });
+}
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
