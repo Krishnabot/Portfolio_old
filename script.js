158eb9f6349a316fa0c651f3653a560a27c0f962
Krishna <libraryismyparadise@gmail.com>
2022-06-16 02:41:13 +0545

    Fix Linter error

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navBar = document.querySelector(".nav-bar");
-const navLink = document.querySelector(".nav-link");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navBar = document.querySelector('.nav-bar');
+const navLink = document.querySelector('.nav-link');
 function myFunction() {
-  const collection = document.querySelector(".nav-item").children;
+  const collection = document.querySelector('.nav-item').children;
   const l = collection.length;
-  for (let i = 0; i <= l; i++) {
-    hamburger.addEventListener("click", () => {
-      navLink.classList.toggle("active");
-      console.log("itworks");
+  for (let i = 0; i <= l; i += 1) {
+    hamburger.addEventListener('click', () => {
+      navLink.classList.toggle('active');
     });
   }
 }
 if (hamburger) {
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    navBar.classList.toggle("active");
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    navBar.classList.toggle('active');
     myFunction();
   });
 }
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    navBar.classList.remove("active");
-    navLink.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  navBar.classList.remove('active');
+  navLink.classList.remove('active');
+}));
